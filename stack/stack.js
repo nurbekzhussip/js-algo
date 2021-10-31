1cf9f55751af1f23f8a9a3601d118dd8d4bbdfbd
Nurbek Zhussip <NAZhusip@sber-solutions.kz>
2021-10-31 18:22:55 +0600

    feat: add stack

---
+++ stack/stack.js
@@ -0,0 +1,22 @@
+var isValid = function (s) {
+  let stack = [];
+  let arr = s.split("");
+
+  const bracketTypes = {
+    ")": "(",
+    "]": "[",
+    "}": "{",
+  };
+
+  for (let i = 0; i < arr.length; i++) {
+    if (!bracketTypes[arr[i]]) {
+      stack.push(arr[i]);
+    } else {
+      if (bracketTypes[arr[i]] !== stack.pop()) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
+};
